40221b4fe2da002051f923b54244f81906c69b9e
jordangm94 <jordan.guerrero.martinez@gmail.com>
2025-02-27 13:40:44 -0500

    Remove unneeded box with height and width due to new dark mode component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-import { Box } from "@mui/material";
 import DarkMode from "./components/DarkMode";
 
 export default function Home() {
-  // const theme = useTheme();
-  return (
-    <Box
-      sx={{
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <DarkMode />
-    </Box>
-  );
+  return <DarkMode />;
 }
