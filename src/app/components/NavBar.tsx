61730e36c91c58ecf4b0a351cecc7d55535ec457
jordangm94 <jordan.guerrero.martinez@gmail.com>
2025-03-04 14:05:04 -0500

    Add mobile navigation menu for smaller screen sizes

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,10 +4,21 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { useTheme } from "@mui/material";
+import { IconButton, Menu, MenuItem, useTheme } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function NavBar() {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const navTypographyStyles = {
     flexGrow: 1,
@@ -54,7 +65,8 @@ export default function NavBar() {
                 .dev
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", cursor: "" }}>
+            {/* Desktop Navigation*/}
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
               <Typography component="div" sx={navTypographyStyles}>
                 Home
               </Typography>
@@ -70,6 +82,43 @@ export default function NavBar() {
               <Typography component="div" sx={navTypographyStyles}>
                 Contact
               </Typography>
+            </Box>
+            {/* Mobile Navigation */}
+            <Box sx={{ display: { xs: "block", md: "none" } }}>
+              <IconButton
+                color="inherit"
+                aria-label="menu"
+                onClick={handleOpen}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                  paper: {
+                    sx: {
+                      display: "flex",
+                      justifyContent: "center",
+                      backgroundImage: "none",
+                      backgroundColor: theme.palette.background.paper,
+                      color: theme.palette.text.primary,
+                      borderRadius: "8px",
+                    },
+                  },
+                }}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+              >
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+                <MenuItem onClick={handleClose}>About</MenuItem>
+                <MenuItem onClick={handleClose}>Skills</MenuItem>
+                <MenuItem onClick={handleClose}>Projects</MenuItem>
+                <MenuItem onClick={handleClose}>Contact</MenuItem>
+              </Menu>
             </Box>
           </Box>
         </Toolbar>
